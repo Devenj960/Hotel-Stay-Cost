2956c187c337811a069fece5c7c24abef48a2060
Deven Johnson <devenj960@gmail.com>
2025-04-22 15:16:07 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Program_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numGuestsLabel = new System.Windows.Forms.Label();
+            this.numNightsLabel = new System.Windows.Forms.Label();
+            this.numStarsLabel = new System.Windows.Forms.Label();
+            this.hotelCostLabel = new System.Windows.Forms.Label();
+            this.hotelCostOutput = new System.Windows.Forms.Label();
+            this.numGuestsTextbox = new System.Windows.Forms.TextBox();
+            this.numNightsTextbox = new System.Windows.Forms.TextBox();
+            this.numStarsTextbox = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numGuestsLabel
+            // 
+            this.numGuestsLabel.AutoSize = true;
+            this.numGuestsLabel.Location = new System.Drawing.Point(117, 90);
+            this.numGuestsLabel.Name = "numGuestsLabel";
+            this.numGuestsLabel.Size = new System.Drawing.Size(143, 19);
+            this.numGuestsLabel.TabIndex = 0;
+            this.numGuestsLabel.Text = "Number of Guests:";
+            // 
+            // numNightsLabel
+            // 
+            this.numNightsLabel.AutoSize = true;
+            this.numNightsLabel.Location = new System.Drawing.Point(117, 128);
+            this.numNightsLabel.Name = "numNightsLabel";
+            this.numNightsLabel.Size = new System.Drawing.Size(141, 19);
+            this.numNightsLabel.TabIndex = 1;
+            this.numNightsLabel.Text = "Number of Nights:";
+            // 
+            // numStarsLabel
+            // 
+            this.numStarsLabel.AutoSize = true;
+            this.numStarsLabel.Location = new System.Drawing.Point(117, 167);
+            this.numStarsLabel.Name = "numStarsLabel";
+            this.numStarsLabel.Size = new System.Drawing.Size(92, 19);
+            this.numStarsLabel.TabIndex = 2;
+            this.numStarsLabel.Text = "Hotel Stars:";
+            // 
+            // hotelCostLabel
+            // 
+            this.hotelCostLabel.AutoSize = true;
+            this.hotelCostLabel.Location = new System.Drawing.Point(117, 281);
+            this.hotelCostLabel.Name = "hotelCostLabel";
+            this.hotelCostLabel.Size = new System.Drawing.Size(88, 19);
+            this.hotelCostLabel.TabIndex = 3;
+            this.hotelCostLabel.Text = "Hotel Cost:";
+            // 
+            // hotelCostOutput
+            // 
+            this.hotelCostOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.hotelCostOutput.Location = new System.Drawing.Point(323, 273);
+            this.hotelCostOutput.Name = "hotelCostOutput";
+            this.hotelCostOutput.Size = new System.Drawing.Size(100, 27);
+            this.hotelCostOutput.TabIndex = 4;
+            // 
+            // numGuestsTextbox
+            // 
+            this.numGuestsTextbox.Location = new System.Drawing.Point(323, 90);
+            this.numGuestsTextbox.Name = "numGuestsTextbox";
+            this.numGuestsTextbox.Size = new System.Drawing.Size(100, 27);
+            this.numGuestsTextbox.TabIndex = 5;
+            // 
+            // numNightsTextbox
+            // 
+            this.numNightsTextbox.Location = new System.Drawing.Point(323, 128);
+            this.numNightsTextbox.Name = "numNightsTextbox";
+            this.numNightsTextbox.Size = new System.Drawing.Size(100, 27);
+            this.numNightsTextbox.TabIndex = 6;
+            // 
+            // numStarsTextbox
+            // 
+            this.numStarsTextbox.Location = new System.Drawing.Point(323, 167);
+            this.numStarsTextbox.Name = "numStarsTextbox";
+            this.numStarsTextbox.Size = new System.Drawing.Size(100, 27);
+            this.numStarsTextbox.TabIndex = 7;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(207, 220);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(97, 34);
+            this.calcButton.TabIndex = 8;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.calcButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.numStarsTextbox);
+            this.Controls.Add(this.numNightsTextbox);
+            this.Controls.Add(this.numGuestsTextbox);
+            this.Controls.Add(this.hotelCostOutput);
+            this.Controls.Add(this.hotelCostLabel);
+            this.Controls.Add(this.numStarsLabel);
+            this.Controls.Add(this.numNightsLabel);
+            this.Controls.Add(this.numGuestsLabel);
+            this.Name = "Form1";
+            this.Text = "Program 2 - Hotel Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numGuestsLabel;
+        private System.Windows.Forms.Label numNightsLabel;
+        private System.Windows.Forms.Label numStarsLabel;
+        private System.Windows.Forms.Label hotelCostLabel;
+        private System.Windows.Forms.Label hotelCostOutput;
+        private System.Windows.Forms.TextBox numGuestsTextbox;
+        private System.Windows.Forms.TextBox numNightsTextbox;
+        private System.Windows.Forms.TextBox numStarsTextbox;
+        private System.Windows.Forms.Button calcButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿/*
+S2217
+Program 2
+Due Date: 03/11/2022
+Course Section: CIS 199-03
+Displays the cost of a hotel stay based on
+the number of guests, duration of the stay,
+and star rating of the hotel.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            const int LOWEST_GUEST = 100; //Constant for 1 guest fee
+            const int LOW_GUEST = 150; //Constant for 2 guest fee
+            const int HIGH_GUEST = 250; //Constant for 3 guest fee
+            const int HIGHEST_GUEST = 400; //Constant for 4-7 guest fee
+            const int DAILY_RATE = 100; //Constantt for 1-6 nights
+            const int WEEKLY_RATE = 75; //Constant for 7-30 nights
+            const int MONTHLY_RATE = 25; //Constant for 31+ nights
+            const double ONE_STAR = 1; //One star multiplier
+            const double TWO_STAR = 1.5; //Two star multiplier
+            const double THREE_STAR = 2.5; //Three star multipler
+            const double FOUR_STAR = 3; //Four star multiplier
+            const double FIVE_STAR = 4; //Five star multiplier
+            int numGuests; //Number of guests
+            int numNights = 0; //Number of nights
+            int numStars; //Number of stars
+            double starCostFactor = 0; //Assigns star multiplier
+            int guestFactor = 0; //Assigns guest fee
+            int nightsFactor = 0; //Assigns night rate
+            int nightCost = 0; //Cost per night
+            double totalCost; //Total cost
+
+
+            if (int.TryParse(numGuestsTextbox.Text, out numGuests) && numGuests >= 1 && numGuests <= 7) //TryParse for number of guests
+            {
+                if (int.TryParse(numNightsTextbox.Text, out numNights) && numNights >= 1) //TryParse for number of nights
+                {
+                    if (int.TryParse(numStarsTextbox.Text, out numStars) && numStars >= 1 && numStars <= 5) //TryParse for number of stars
+                    {
+                        if (numStars == 5) //Assigns a star multiplier based on how many stars were input
+                            starCostFactor = FIVE_STAR;
+                        else if (numStars == 4)
+                            starCostFactor = FOUR_STAR;
+                        else if (numStars == 3)
+                            starCostFactor = THREE_STAR;
+                        else if (numStars == 2)
+                            starCostFactor = TWO_STAR;
+                        else
+                            starCostFactor = ONE_STAR;
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("Number of stars not valid."); //Displays error message if TryParse fails, clears the textbox, and focuses it
+                        numStarsTextbox.Clear();
+                        numStarsTextbox.Focus();
+                    }
+
+                    if (numNights >= 31) //Assigns nightly rate based on how many nights were input
+                        nightsFactor = MONTHLY_RATE;
+                    else if (numNights <= 30 && numNights >= 7)
+                        nightsFactor = WEEKLY_RATE;
+                    else
+                        nightsFactor = DAILY_RATE;
+                }
+
+                else
+                {
+                    MessageBox.Show("Number of nights not valid."); //Displays error message if TryParse fails, clears the textbox, and focuses it
+                    numNightsTextbox.Clear();
+                    numNightsTextbox.Focus();
+                }
+
+                if (numGuests <= 7 && numGuests >= 4) //Assigns a guest fee based on how many guests were input
+                    guestFactor = HIGHEST_GUEST;
+                else if (numGuests == 3)
+                    guestFactor = HIGH_GUEST;
+                else if (numGuests == 2)
+                    guestFactor = LOW_GUEST;
+                else
+                    guestFactor = LOWEST_GUEST;
+            }
+
+            else
+            {
+                MessageBox.Show("Number of guests not valid."); //Displays error message if TryParse fails, clears the textbox, and focuses
+                numGuestsTextbox.Clear();
+                numGuestsTextbox.Focus();
+            }
+
+            nightCost = numNights * nightsFactor; //Finds and assigns the night cost amount
+            totalCost = (nightCost + guestFactor) * starCostFactor; //Finds and assigns the total cost amount
+
+            hotelCostOutput.Text = totalCost.ToString("C"); //Displays total cost
+        }
+    }
+}
